89f80f427131ff9ef598026d339cbcce8e4956ef
Southball <jamie.choi.mail.2002@gmail.com>
2020-08-08 18:04:41 +0800

    Update apply script

--- apply.sh
+++ apply.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # NeoVim
 mkdir -p ~/.config/nvim
 cp $DIR/nvim/init.vim ~/.config/nvim/init.vim
-cp $DIR/nvim/coc.vim ~/.cofig/nvim/init.vim
+cp $DIR/nvim/coc.vim ~/.config/nvim/coc.vim
 
