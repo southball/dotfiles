e569501c39b21cc70404928646e7901b6401f5f6
Southball <jamie.choi.mail.2002@gmail.com>
2021-08-24 18:06:00 +0900

    Add Emacs init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,28 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package '(proof-general company-coq evil))
+   (unless (package-installed-p package)
+       (package-install package)))
+
+(add-hook 'coq-mode-hook #'company-coq-mode)
+
+(require 'evil)
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil company-coq proof-general))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
